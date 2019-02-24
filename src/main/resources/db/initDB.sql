a265cd24e7f7ad65149144f15939b91c1efd1da8
Viktor <vreztsov90@mail.ru>
2019-02-24 23:35:55 +0300

    HW03 add Spring JDBC Template

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,11 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS meal_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
+CREATE SEQUENCE meal_seq;
 
 CREATE TABLE users
 (
@@ -21,5 +24,15 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  user_id INTEGER NOT NULL,
+  id               INTEGER PRIMARY KEY DEFAULT nextval('meal_seq'),
+  datetime         TIMESTAMP DEFAULT now() NOT NULL,
+  description      VARCHAR                 NOT NULL,
+  calories         INTEGER DEFAULT 0       NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE meal_seq RESTART WITH 1;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +11,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, id, datetime, description, calories) VALUES
+(100000, nextval('meal_seq'::regclass), '2015-05-30 10:00', 'Завтрак', 500),
+(100000, nextval('meal_seq'::regclass), '2015-05-30 13:00', 'Обед', 1000),
+(100000, nextval('meal_seq'::regclass), '2015-05-30 20:00', 'Ужин', 500),
+(100000, nextval('meal_seq'::regclass), '2015-05-31 10:00', 'Завтрак', 1000),
+(100000, nextval('meal_seq'::regclass), '2015-05-31 13:00', 'Обед', 500),
+(100000, nextval('meal_seq'::regclass), '2015-05-31 20:00', 'Ужин', 510);
