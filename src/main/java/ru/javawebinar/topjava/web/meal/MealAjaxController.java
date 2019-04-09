41fc87a5e9e4ee2b191310c7a04b40c5407bf9fc
Viktor <vreztsov90@mail.ru>
2019-04-09 21:51:07 +0300

    HW08 basic part

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -0,0 +1,45 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.to.MealTo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ajax/meals")
+public class MealAjaxController extends AbstractMealController {
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void createOrUpdate(@RequestParam Integer id,
+                               @RequestParam
+                               @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime dateTime,
+                               @RequestParam String description,
+                               @RequestParam int calories) {
+        Meal meal = new Meal(id, dateTime, description, calories);
+        if (meal.isNew()) {
+            super.create(meal);
+        } else {
+            super.update(meal, id);
+        }
+    }
+}
---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,37 @@
+$(function () {
+    makeEditable({
+            entity: "meal",
+            ajaxUrl: "ajax/meals/",
+            formColumns: ["dateTime","description","calories"],
+            datatableApi: $("#datatable").DataTable({
+                "paging": false,
+                "info": true,
+                "columns": [
+                    {
+                        "data": "dateTime"
+                    },
+                    {
+                        "data": "description"
+                    },
+                    {
+                        "data": "calories"
+                    },
+                    {
+                        "defaultContent": "Edit",
+                        "orderable": false
+                    },
+                    {
+                        "defaultContent": "Delete",
+                        "orderable": false
+                    }
+                ],
+                "order": [
+                    [
+                        0,
+                        "desc"
+                    ]
+                ]
+            })
+        }
+    );
+});
